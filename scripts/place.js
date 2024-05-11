37b097b857b333364e66403e3d8985a45a295086
Mdetlefsen77 <martin.detle@gmail.com>
2024-05-11 15:15:36 -0300

    Assigment of week 03 is done

---
+++ scripts/place.js
@@ -0,0 +1,13 @@
+const windchill = document.querySelector('#wind-chill');
+const temperature = 32;
+const windspeed = 18;
+function calculateWindChill(temperature, windspeed) {
+    const chill = 35.74 + (0.6215 * temperature) - 35.75 * (Math.pow(windspeed, 0.16)) + 0.4275 * temperature * (Math.pow(windspeed, 0.16));
+    return chill;
+}
+const windchillNumber = calculateWindChill(temperature, windspeed);
+if (temperature <= 10 && windspeed > 4.8) {
+    windchill.textContent = windchillNumber.toFixed(2);
+} else {
+    windchill.textContent = "N/A";
+}
